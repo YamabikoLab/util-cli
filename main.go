a6d31aca4f84336117feb963ba43085d56263de3
Yoshifumi Hijikata <yhijikata@systemlancer.com>
2024-02-01 06:16:13 +0900

    設定ファイルを追加した

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
-	"strconv"
 	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/xuri/excelize/v2"
+	"gopkg.in/yaml.v2"
 )
+
+type Config struct {
+	Keywords   []string   `yaml:"keywords"`
+	Regex      string     `yaml:"regex"`
+	Exclusions Exclusions `yaml:"exclusions"`
+	Options    string     `yaml:"options"`
+}
+
+type Exclusions struct {
+	Directories []string `yaml:"directories"`
+	Files       []string `yaml:"files"`
+}
 
 func main() {
 	var utCmd = &cobra.Command{
@@ -21,23 +35,53 @@ func main() {
 		Short:              "egrep command",
 		DisableFlagParsing: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			out, err := exec.Command("egrep", args...).CombinedOutput()
+			config := Config{}
+
+			data, err := os.ReadFile("config.yml")
+			if err != nil {
+				return err
+			}
+
+			err = yaml.UnmarshalStrict(data, &config)
 			if err != nil {
 				return err
 			}
 
 			f := excelize.NewFile()
 
-			lines := strings.Split(string(out), "\n")
-			for i, line := range lines {
-				// Excel file indexes start from 1, not 0
-				f.SetCellValue("Sheet1", "A"+strconv.Itoa(i+1), line)
+			excludedDirs := ""
+			for _, dir := range config.Exclusions.Directories {
+				excludedDirs += fmt.Sprintf(" --exclude-dir=%s", dir)
+			}
+
+			excludedFiles := ""
+			for _, file := range config.Exclusions.Files {
+				excludedFiles += fmt.Sprintf(" --exclude=%s", file)
+			}
+
+			for _, keyword := range config.Keywords {
+				replacedRegex := strings.ReplaceAll(config.Regex, "{key}", keyword)
+				out, err := exec.Command("bash", "-c", fmt.Sprintf("egrep %s '%s' ./ %s %s", config.Options, replacedRegex, excludedDirs, excludedFiles)).Output()
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err.Error())
+					continue
+				}
+
+				_, err = f.NewSheet(keyword)
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err.Error())
+					continue
+				}
+
+				lines := strings.Split(string(out), "\n")
+				for i := 1; i <= len(lines); i++ {
+					_ = f.SetCellValue(keyword, fmt.Sprintf("A%d", i), lines[i-1])
+				}
 			}
 
 			if err := f.SaveAs("EgrepResults.xlsx"); err != nil {
 				return err
 			}
-
 			return nil
 		},
 	}
